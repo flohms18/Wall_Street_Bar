985b2761fe86bf5353434373f42a745c6d6deebd
flohms18 <trouilletflorent@outlook.com>
2025-04-28 11:27:06 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+Menu_WS = {
+    "Draft Bear" : {
+        'Goose IPA' : 9.9,
+        'Ginette Blanche' : 7,
+        'Kwak fruit rouge' : 10,
+        'Triple Karmeliet' : 9.9,
+    },
+    "Bottles Beer" : {
+        "Chouffe" : 6.6,
+        "Parisis Ambre" : 5,
+        "Chouf Cherry" : 4,
+    },
+}
+
+print(Menu_WS["Bottles Beer"]['Chouffe'])
